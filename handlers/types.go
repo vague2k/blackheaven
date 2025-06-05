8ad9ca95de875d8cc080b085eb5f3da69dca519f
vague2k <ilovedrawing056@gmail.com>
2025-06-05 15:56:47 -0400

    refactor: move helpers/type to a single utils file

--- handlers/types.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-var (
-	ErrInternal            = "Internal Server Error"
-	ErrInquiryKindEmpty    = "inquiry kind can't be empty"
-	ErrInquiryEmailEmpty   = "inquiry email can't be empty"
-	ErrInquiryEmailInvalid = "inquiry email is invalid"
-	ErrInquiryContentEmpty = "inquiry content can't be empty"
-)
--- handlers/utils.go
+++ handlers/utils.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+var (
+	ErrInternal            = "Internal Server Error"
+	ErrInquiryKindEmpty    = "inquiry kind can't be empty"
+	ErrInquiryEmailEmpty   = "inquiry email can't be empty"
+	ErrInquiryEmailInvalid = "inquiry email is invalid"
+	ErrInquiryContentEmpty = "inquiry content can't be empty"
+)
+
 type RespErr struct {
 	Status   int    `json:"status"`
 	ErrorMsg string `json:"content"`
