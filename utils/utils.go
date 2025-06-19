173f9d2c200531877c63de01c70552b97d3af901
vague2k <ilovedrawing056@gmail.com>
2025-06-19 17:41:50 -0400

    fix: capitlize topic badge text

--- utils/utils.go
+++ utils/utils.go
@@ -2,39 +2,18 @@ package utils
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"runtime"
+	"unicode"
 )
-
-// Gets the user's $XDG_DATA_HOME dir.
-//
-// Fallsback to the default data dir if the env var does not exist.
-func UserDataDir() string {
-	var dataDir string
-
-	if dataDir = os.Getenv("XDG_DATA_HOME"); dataDir != "" {
-		return dataDir
-	}
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal("Could not get the home directory")
-	}
-
-	switch runtime.GOOS {
-	case "windows":
-		if dataDir = os.Getenv("LOCALAPPDATA"); dataDir != "" {
-			dataDir = filepath.Join(home, "AppData", "Local")
-		}
-	default:
-		dataDir = filepath.Join(home, ".local", "share")
-	}
-
-	return dataDir
-}
 
 func ID(formID, name, component, element string) string {
 	return fmt.Sprintf("%s-%s-%s-%s", formID, name, component, element)
+}
+
+func Capitalize(s string) string {
+	if s == "" {
+		return ""
+	}
+	runes := []rune(s)
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
 }
